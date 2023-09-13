6a1394ab59efe5d2cddfde77d2eed2b7c27188e7
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-09-14 00:58:03 +0100

    add Store to Meeting

--- script/index.js
+++ script/index.js
@@ -3,6 +3,6 @@
 	const title = document.title;
 	const id = location.pathname.slice(1);
 	const info = await Store.findOrCreateMeeting(id, title, true);
-	const meeting = new Meeting(info, options);
+	const meeting = new Meeting(info, options, Store);
 	meeting.start();
 })();
