cd338c342b56c6c386ccdf142409fa693e5a42fc
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-10-06 18:20:46 +0100

    rework popup to use store events

--- popup/buttons.js
+++ popup/buttons.js
@@ -1,90 +1,104 @@
-const optionsButton = /** @type {HTMLElement} */ (document.getElementById("options"));
-const optionsOKButton = /** @type {HTMLElement} */ (document.querySelector(".options .ok"));
-const optionsView = /** @type {HTMLCollectionOf<HTMLElement>} */ (document.getElementsByClassName("option"));
-const tableIcons = /** @type {NodeListOf<HTMLElement>} */ (document.querySelectorAll("table th"));
-const csvButton = /** @type {HTMLElement} */ (document.querySelector(".buttons .csv"));
-const jsonButton = /** @type {HTMLElement} */ (document.querySelector(".buttons .json"));
+function bindOptionsButtons() {
+	const optionsButton = /** @type {HTMLElement} */ (document.getElementById("options"));
+	const optionsOKButton = /** @type {HTMLElement} */ (document.querySelector(".options .ok"));
+	const containerNode = /** @type {HTMLElement} */ (document.querySelector(".container"));
+	const optionsView = /** @type {HTMLCollectionOf<HTMLElement>} */ (document.getElementsByClassName("option"));
 
-optionsButton.onclick = () => {
-	containerNode.classList.toggle("has-options");
-};
+	optionsButton.onclick = () => {
+		containerNode.classList.toggle("has-options");
+	};
+	
+	optionsOKButton.onclick = () => {
+		containerNode.classList.toggle("has-options");
+	};
 
-optionsOKButton.onclick = () => {
-    containerNode.classList.toggle("has-options");
-};
-
-for(const icon of tableIcons) {
-	icon.onclick = () => {
-		if(meetingNode.dataset.sort === icon.dataset.sort) {
-			const old = meetingNode.dataset.reverse;
-			meetingNode.dataset.reverse = old === "true" ? "false" : "true";
-			icon.dataset.ui = old === "true" ? "▼" : "▲";
-		} else {
-			meetingNode.dataset.sort = icon.dataset.sort;
-			meetingNode.dataset.reverse = "false";
-			icon.dataset.ui = "▼";
-			for(const i of tableIcons) {
-				if(i === icon) {
-					continue;
+	Store.getOptions().then(options => {
+		for(const optionNode of optionsView) {
+			const key = optionNode.dataset.option;
+			const button = /** @type {HTMLElement} */ (optionNode.children[1]);
+			button.textContent = options[key] ? "toggle_on" : "toggle_off";
+			optionNode.dataset.enabled = Boolean(options[key]).toString();
+			button.onclick = () => {
+				const oldstate = optionNode.dataset.enabled;
+				optionNode.dataset.enabled = oldstate === "true" ? "false" : "true";
+				button.textContent = oldstate === "false" ? "toggle_on" : "toggle_off";
+				if((oldstate === "false" && !options[key]) || (oldstate === "true" && options[key])) {
+					options[key] = !options[key];
+					Store.setOptions(options).catch(console.error);
 				}
-				i.dataset.ui = "";
 			}
 		}
-	}
+	});
 }
 
-csvButton.onclick = () => {
-	let csv = `sep=,\n`;
-	csv += Array.prototype.map.call(tableNode.querySelectorAll("tr th span"), x => `"${x.getAttribute("title")}"`).join(",") + "\n";
-	for(const row of tableView) {
-		csv += Array.prototype.map.call(row.querySelectorAll("td"), x => `"${x.innerText}"`).join(",") + "\n";
-	}
-	const link = document.createElement('a');
-	link.href = `data:text/plain,${csv}`;
-	link.download = `${titleNode.textContent}.csv`;
-	link.dispatchEvent(new MouseEvent('click', { 
-		bubbles: true, 
-		cancelable: true, 
-		view: window 
-	}));
-}
+function bindDowloadButtons() {
+	const csvButton = /** @type {HTMLElement} */ (document.querySelector(".buttons .csv"));
+	const jsonButton = /** @type {HTMLElement} */ (document.querySelector(".buttons .json"));
+	const titleNode = /** @type {HTMLElement} */ (document.querySelector(".meeting .title"));
+	const tableNode = /** @type {HTMLElement} */ (document.querySelector(".meeting table tbody"));
+	const tableView = /** @type {HTMLCollectionOf<HTMLElement>} */ (document.getElementsByClassName("participant"));
 
-jsonButton.onclick = async () => {
-	const meeting = await Utils.getCurrentMeeting();
-	if(meeting) {
-		const participants = await Store.listMeetingParticipants(meeting.dataId);
-		const participantsData = await Store.getMultipleParticipantsData(meeting.dataId, participants.map(x => x.dataId));
-		meeting["participants"] = participants;
-		for(const participant of participants) {
-			participant["data"] = Utils.parseData(participantsData[participant.dataId]);
-			participant["data"].time = participant.lastSeen - participant.firstSeen;
-			participant["events"] = participantsData[participant.dataId];
+	csvButton.onclick = () => {
+		let csv = `sep=,\n`;
+		csv += Array.prototype.map.call(tableNode.querySelectorAll("tr th span"), x => `"${x.getAttribute("title")}"`).join(",") + "\n";
+		for(const row of tableView) {
+			csv += Array.prototype.map.call(row.querySelectorAll("td"), x => `"${x.innerText}"`).join(",") + "\n";
 		}
 		const link = document.createElement('a');
-		link.href = `data:text/plain,${JSON.stringify(meeting)}`;
-		link.download = `${titleNode.textContent}.json`;
+		link.href = `data:text/plain,${csv}`;
+		link.download = `${titleNode.textContent}.csv`;
 		link.dispatchEvent(new MouseEvent('click', { 
 			bubbles: true, 
 			cancelable: true, 
 			view: window 
 		}));
 	}
-}
-
-Store.getOptions().then(options => {
-	for(const optionNode of optionsView) {
-		const key = optionNode.dataset.option;
-		const button = /** @type {HTMLElement} */ (optionNode.children[1]);
-		button.textContent = options[key] ? "toggle_on" : "toggle_off";
-		optionNode.dataset.enabled = Boolean(options[key]).toString();
-		button.onclick = () => {
-			const oldstate = optionNode.dataset.enabled;
-			optionNode.dataset.enabled = oldstate === "true" ? "false" : "true";
-			button.textContent = oldstate === "false" ? "toggle_on" : "toggle_off";
-			if((oldstate === "false" && !options[key]) || (oldstate === "true" && options[key])) {
-				options[key] = !options[key];
-				Store.setOptions(options).catch(console.error);
+	
+	jsonButton.onclick = async () => {
+		const meeting = await Utils.getCurrentMeeting();
+		if(meeting) {
+			const participants = await Store.listMeetingParticipants(meeting.dataId);
+			const participantsData = await Store.getMultipleParticipantsData(meeting.dataId, participants.map(x => x.dataId));
+			meeting["participants"] = participants;
+			for(const participant of participants) {
+				participant["data"] = Utils.parseData(participantsData[participant.dataId]);
+				participant["data"].time = participant.lastSeen - participant.firstSeen;
+				participant["events"] = participantsData[participant.dataId];
 			}
+			const link = document.createElement('a');
+			link.href = `data:text/plain,${JSON.stringify(meeting)}`;
+			link.download = `${titleNode.textContent}.json`;
+			link.dispatchEvent(new MouseEvent('click', { 
+				bubbles: true, 
+				cancelable: true, 
+				view: window 
+			}));
 		}
 	}
-});
+}
+
+function bindTableButtons(collection) {
+	const tableIcons = /** @type {NodeListOf<HTMLElement>} */ (document.querySelectorAll("table th"));
+	const meetingNode = /** @type {HTMLElement} */ (document.querySelector(".meeting"));
+
+	for(const icon of tableIcons) {
+		icon.onclick = () => {
+			if(meetingNode.dataset.sort === icon.dataset.sort) {
+				const old = meetingNode.dataset.reverse;
+				meetingNode.dataset.reverse = old === "true" ? "false" : "true";
+				icon.dataset.ui = old === "true" ? "▼" : "▲";
+			} else {
+				meetingNode.dataset.sort = icon.dataset.sort;
+				meetingNode.dataset.reverse = "false";
+				icon.dataset.ui = "▼";
+				for(const i of tableIcons) {
+					if(i === icon) {
+						continue;
+					}
+					i.dataset.ui = "";
+				}
+			}
+			updateTable(collection, meetingNode.dataset.sort, meetingNode.dataset.reverse === "true");
+		}
+	}
+}
--- popup/main.js
+++ popup/main.js
@@ -1,3 +1,71 @@
+
+bindOptionsButtons();
+
+(async () => {
+	const containerNode = /** @type {HTMLElement} */ (document.querySelector(".container"));
+	const meeting = await Utils.getCurrentMeeting();
+	if(meeting) {
+		const meetingNode = /** @type {HTMLElement} */ (document.querySelector(".meeting"));
+		const titleNode = /** @type {HTMLElement} */ (document.querySelector(".meeting .title"));
+		const timeNode = /** @type {HTMLElement} */ (document.querySelector(".meeting .time"));
+
+		containerNode.classList.add("has-meeting");
+		meetingNode.dataset.id = meeting.id;
+		titleNode.textContent = meeting.title;
+		timeNode.textContent = Utils.milliToHHMMSS(meeting.lastSeen - meeting.firstSeen);
+
+		/**
+		 * @type {{
+		 * 		data: CollectionData[],
+		 * 		keys: Record<string, CollectionData>
+		 * }}
+		 */
+		const collection = {
+			data: [],
+			keys: {}
+		};
+
+		const participants = await Store.listMeetingParticipants(meeting.dataId);
+		const participantsData = await Store.getMultipleParticipantsData(meeting.dataId, participants.map(x => x.dataId));
+		for(const participant of participants) {
+			const data = updateParticipant(participant, collection);
+			const updated = Utils.parseData(participantsData[participant.dataId]);
+			updateParticipantData(data, updated);
+		}
+
+		updateTable(collection, meetingNode.dataset.sort, meetingNode.dataset.reverse === "true");
+
+		// @ts-ignore
+		chrome.storage.onChanged.addListener((/** @type {Record<string, { oldValue: *, newValue: * }>} */ changes, /** @type {string} */ namespace) => {
+			if(namespace === "local") {
+				for(const id in changes) {
+					if(id === "list") {
+						const found = changes.list.newValue.find(x => x.dataId === meeting.dataId);
+						if(found) {
+							Object.assign(meeting, found);
+							timeNode.textContent = Utils.milliToHHMMSS(meeting.lastSeen - meeting.firstSeen);
+						}
+					} else if(id[0] === "P") {
+						for(const participant of changes[id].newValue) {
+							updateParticipant(participant, collection);
+						}
+					} else if(id[0] === "D") {
+						const dataId = id.split("-")[2];
+						const participant = collection.keys[dataId];
+						const updated = Utils.parseData(changes[id].newValue.map(x => Store.decodeEvent(x)));
+						updateParticipantData(participant, updated);
+					}
+				}
+				updateTable(collection, meetingNode.dataset.sort, meetingNode.dataset.reverse === "true");
+			}
+		});
+
+		bindDowloadButtons();
+		bindTableButtons(collection);
+
+	}
+})();
+
 /**
  * @typedef {{
  * 		name: string,
@@ -14,98 +82,3 @@
  * 		node: HTMLElement
  * }} CollectionData
  */
-
-/**
- * @type {{
- * 		data: CollectionData[],
- * 		keys: Record<string, CollectionData>
- * }}
- */
-const collection = {
-	data: [],
-	keys: {}
-};
-
-const containerNode = /** @type {HTMLElement} */ (document.querySelector(".container"));
-const meetingNode = /** @type {HTMLElement} */ (document.querySelector(".meeting"));
-const titleNode = /** @type {HTMLElement} */ (document.querySelector(".meeting .title"));
-const timeNode = /** @type {HTMLElement} */ (document.querySelector(".meeting .time"));
-const tableNode = /** @type {HTMLElement} */ (document.querySelector(".meeting table tbody"));
-const tableView = /** @type {HTMLCollectionOf<HTMLElement>} */ (document.getElementsByClassName("participant"));
-
-loop();
-
-let globalInterval = setInterval(loop, 1000);
-
-async function loop() {
-	const meeting = await Utils.getCurrentMeeting();
-	if(meeting) {
-		if(!containerNode.classList.contains("has-meeting")) {
-			containerNode.classList.add("has-meeting");
-		}
-		if(!meetingNode.dataset.id) {
-			meetingNode.dataset.id = meeting.id;
-			titleNode.textContent = meeting.title;
-		}
-		if(meetingNode.dataset.id !== meeting.id) {
-			meetingNode.dataset.id = meeting.id;
-			titleNode.textContent = meeting.title;
-			tableNode.innerHTML = tableNode.firstElementChild?.outerHTML || "";
-		}
-		
-		timeNode.textContent = Utils.milliToHHMMSS(meeting.lastSeen - meeting.firstSeen);
-		
-		const participants = await Store.listMeetingParticipants(meeting.dataId);
-		const participantsData = await Store.getMultipleParticipantsData(meeting.dataId, participants.map(x => x.dataId));
-		
-		for(const participant of participants) {
-			let data = collection.keys[participant.dataId];
-			if(!data) {
-				const d = Utils.createNode(participant);
-				collection.data.push(d);
-				data = collection.keys[participant.dataId] = d;
-			}
-			const updated = Utils.parseData(participantsData[participant.dataId]);
-			updated["time"] = participant.lastSeen - participant.firstSeen;
-			const keys = ["time", "cam", "mic", "voice", "presentation", "hands", "emojis", "texts"];
-			for(let i = 0; i < keys.length; i++) {
-				const key = keys[i];
-				if(updated[key] !== data.calculated[key]) {
-					data.calculated[key] = updated[key];
-					data.node.children[i + 1].textContent = ["time", "cam", "mic", "voice", "presentation"].includes(key) ? Utils.milliToHHMMSS(updated[key]) : updated[key].toString();
-				}
-			}
-		}
-
-		if(meetingNode.dataset.sort) {
-			const k = meetingNode.dataset.sort;
-			collection.data.sort((a,b) => {
-				const r = b.calculated[k] - a.calculated[k];
-				if(r === 0) {
-					return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-				}
-				return r;
-			});
-		} else {
-			collection.data.sort((a,b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
-		}
-
-		if(meetingNode.dataset.reverse === "true") {
-			collection.data.reverse();
-		}
-
-		for(let i = 0; i < collection.data.length; i++) {
-			const cnode = collection.data[i].node;
-			const vnode = tableView[i];
-			if(vnode && cnode !== vnode) {
-				tableNode.insertBefore(cnode, vnode);
-			} else if(!vnode) {
-				tableNode.appendChild(cnode);
-			}
-		}
-	} else {
-		if(containerNode.classList.contains("has-meeting")) {
-			containerNode.classList.remove("has-meeting");
-		}
-	}
-}
