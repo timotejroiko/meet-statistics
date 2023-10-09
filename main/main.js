e1ea37e8a6e585f6059a60db12e9440b0079e44d
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-10-09 01:48:45 +0100

    WIP main UI

--- main/main.js
+++ main/main.js
@@ -3,21 +3,34 @@ router();
 load();
 
 function load() {
-    const active = document.querySelector(".show")?.id;
-    switch(active) {
-        case "main": return loadMain();
-        case "meeting": return loadMain();
-        case "participant": return loadMain();
+    const active = document.querySelector(".show");
+    if(active && !active.classList.contains("loaded")) {
+        switch(active.id) {
+            case "main": return loadMain();
+            case "meeting": return loadMain();
+            case "participant": return loadMain();
+        }
     }
 }
 
 async function loadMain() {
-    const statsNode = /** @type {HTMLElement} */ (document.getElementById("main")?.querySelector(".sidebar .lower .stats"));
+    const mainNode = /** @type {HTMLElement} */ (document.getElementById("main"));
+    const statsNode = /** @type {HTMLElement} */ (mainNode.querySelector(".sidebar .lower .stats"));
+    const tableNode = /** @type {HTMLElement} */ (mainNode.querySelector(".content .container table"));
     // @ts-ignore
     const stats = await chrome.storage.local.getBytesInUse();
     const meetings = await Store.listMeetings();
     statsNode.children[0].children[1].textContent = meetings.length.toString();
     statsNode.children[1].children[1].textContent = `${(stats / 1024).toFixed(2)}KB`;
-    for(const meeting of meetings) {
+    for(const meeting of meetings.reverse()) {
+        const participants = await Store.listMeetingParticipants(meeting.dataId);
+        const date = new Date(meeting.firstSeen);
+        const d1 = date.toLocaleDateString();
+        const d2 = date.toLocaleTimeString();
+        const d3 = Utils.milliToHHMMSSFull(meeting.lastSeen - meeting.firstSeen);
+        const newNode = document.createElement("tr");
+        newNode.classList.add("participant");
+        newNode.innerHTML = `<td><label><input type="checkbox"/></label></td><td><p>${meeting.title || "N/A"}</p><p>${meeting.id}</p></td><td><p>${d1}</p><p>${d2}</p></td><td><span class="material-symbols-rounded" title="Participants">person</span><p>${participants.length}</p></td><td><p>${d3}</p></td><td></td>`;
+        tableNode.appendChild(newNode);
     }
 }
--- utils/Utils.js
+++ utils/Utils.js
@@ -9,4 +9,15 @@ class Utils {
 		const h = Math.floor(time / 3600);
 		return `${h > 0 ? `${h}:` : ""}${h > 0 ? m.padStart(2, "0") : m}:${s}`;
 	}
+
+	/**
+	* @param {number} milliseconds 
+	*/
+	static milliToHHMMSSFull(milliseconds) {
+		const time = Math.round(milliseconds / 1000);
+		const s = (time % 60).toString().padStart(2, "0");
+		const m = (Math.floor(time / 60) % 60).toString().padStart(2, "0");
+		const h = Math.floor(time / 3600).toString().padStart(2, "0");
+		return `${h}:${m}:${s}`;
+	}
 }
